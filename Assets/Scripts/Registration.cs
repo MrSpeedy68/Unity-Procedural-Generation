2b6a7053ed0ced29c91027b90990decff4c1f5a9
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2022-04-19 15:48:47 +0100

    Working user Registration onto the DB

---
+++ Assets/Scripts/Registration.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Registration : MonoBehaviour
+{
+    private string url = "https://elviveamten.000webhostapp.com/Register.php";
+    
+    public TMP_InputField nameField;
+    public TMP_InputField passwordField;
+    public Button submitButton;
+
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+        WWW www = new WWW(url, form);
+        yield return www;
+        if (www.text == "0")
+        {
+            Debug.Log("user created successfully");
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("User creation failed. Error #" + www.text);
+        }
+    }
+
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
+}
